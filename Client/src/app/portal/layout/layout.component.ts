20d1244b59fb33ddd98f0e8e4e822462cd358ce4
Rohan Sakpal <rohan.sakpal@atriina.com>
2025-03-21 16:38:16 +0530

    Rohan:Create folder structure

---
+++ Client/src/app/portal/layout/layout.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { InfoComponent } from '../info/info.component';
+import { MatDialog } from '@angular/material/dialog';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss']
+})
+export class LayoutComponent {
+  imageBasePath: string = environment.imageBasePath;
+  roleCode: string = '';
+  userName: string = '';
+  constructor(private dialog: MatDialog) {
+    this.roleCode = localStorage.getItem('role') || '';
+    this.userName = localStorage.getItem('userName') || '';
+  }
+
+  logoutModel(event: MouseEvent) {
+    const targetElement = event.target as HTMLElement;
+    const rect = targetElement.getBoundingClientRect();
+    const info = {
+      name: 'profile', title: ''
+    }
+    this.dialog.open(InfoComponent, {
+      position: {
+        top: `${rect.bottom + window.scrollY + 5}px`,
+        left: `${rect.left + window.scrollX - 50}px`,
+      },
+      data: info
+    });
+  }
+}
---
+++ Client/src/app/portal/service/common.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommonService {
+
+  constructor(private http: HttpClient) { }
+  baseUrl = environment.BASE_URL;
+
+  postAPI(url: string, payload: any): Observable<any> {
+    return this.http.post(this.baseUrl + url, payload);
+  }
+
+  getAPI(url: string, userCode?:string): Observable<any> {
+    if(userCode) {
+      let params = new HttpParams().set('userId', userCode)
+      return this.http.get(this.baseUrl + url,{
+        params: params
+      });
+    } else {
+      return this.http.get(this.baseUrl + url);
+    }
+  }
+}
