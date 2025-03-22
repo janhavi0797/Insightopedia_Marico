ca64d959c175f37720c8f0e8652da0d73a387b7e
Vinayak Sakpal <vinayak.sakpal@atriina.com>
2025-03-22 08:53:17 +0530

    Get All and User audio with unique tag

--- server/src/audio/audio.controller.ts
+++ server/src/audio/audio.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AudioService } from './audio.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Audio Management')
 @Controller('audio')
 export class AudioController {
   constructor(private readonly audioService: AudioService) {}
+      @Get('all')
+      @ApiOperation({ summary: 'Get All audio and unique tag' })
+      async getAudio(@Query('userId') userId?: string) {
+          return this.audioService.getAudio(userId);
+      }
 }
